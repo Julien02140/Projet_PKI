6f005fdfac0100d66ad971759514a891654f0c33
Julien Boutreaux <julien.boutreaux@orange.fr>
2024-05-06 16:17:12 +0200

    communication entre CA et client

--- CA.py
+++ CA.py
@@ -1,6 +1,10 @@
 #utiliser pyOpenSSL
 #https://www.youtube.com/watch?v=QhQFEmbRmsY
-
+#pour les communication, en https on peut creer une api flask
+#mais pour le projet, on utilise des fils mqtt
+import paho.mqtt.client as mqtt
+import paho.mqtt
+import ssl, time, inspect, os
 from datetime import datetime, timezone, timedelta
 from cryptography import x509
 from cryptography.x509.oid import NameOID
@@ -9,8 +13,8 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 
-server_IP = '18.224.18.157'
-server_name = 'ca_server'
+#server_IP = '18.224.18.157'
+server_name = 'ca_server_julien_hugo'
 
 key = rsa.generate_private_key(
     public_exponent=65537,
@@ -23,7 +27,7 @@ name = x509.Name([
 ])
 
 alt_names = [x509.DNSName(server_name)]
-alt_names.append(x509.DNSName(server_IP))
+#alt_names.append(x509.DNSName(server_IP))
 
 #elle peut émettre des certificats, si on met path_length=0, elle ne peut pas
 #emettre de certificat, il faut laisser à None
@@ -50,9 +54,52 @@ my_key_pem = key.private_bytes(
     encryption_algorithm=serialization.NoEncryption(),
 )
 
-with open('test_window_new.crt', 'wb') as c:
+with open('ca_cert.crt', 'wb') as c:
     c.write(my_cert_pem)
 
-with open('test_window_new.key', 'wb') as c:
+with open('ca_key.key', 'wb') as c:
     c.write(my_key_pem)
 
+#on doit aussi creer un fichier pem
+#ce fichier contient le certificat et la clé privée
+with open('ca_cert.pem','wb') as c:
+    c.write(my_cert_pem)
+    c.write(my_key_pem)
+
+# nb_message_recu = 0
+
+
+# Paramètres MQTT
+mqtt_broker_address = "194.57.103.203"
+mqtt_broker_port = 1883
+mqtt_client_id = "ca_server_julien_hugo"
+topic = "vehicule/JH"
+
+
+USE_VERSION2_CALLBACKS = not paho.mqtt.__version__.startswith("1.")
+
+if USE_VERSION2_CALLBACKS:
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, mqtt_client_id)
+else:
+    client = mqtt.Client(mqtt_client_id)  
+
+# Connexion au broker MQTT avec TLS/SSL
+print("connecting to broker")
+#client.tls_set(ca_certs="ca_cert.crt", certfile="ca_cert.pem", keyfile="ca_key.key")
+#client.tls_insecure_set(True)
+if client.connect("194.57.103.203",1883,60) != 0:
+    print("Problème de connexion avec le broker")
+
+def on_message(client, userdata, msg):
+    print("Message reçu sur le sujet/topic "+msg.topic+": "+str(msg.payload.decode()))
+    # nb_message_recu = nb_message_recu + 1
+    # print("nb message recu = ", nb_message_recu)
+
+print( "Subscribing to topic",topic)
+client.subscribe("vehicule/JH")
+
+client.on_message=on_message
+
+print("commence à écouter")
+
+client.loop_forever()
--- client.py
+++ client.py
@@ -1,10 +1,11 @@
+import paho.mqtt.client as mqtt
+import paho.mqtt
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
-client_IP = '18.224.18.158'
 client_name = "client1"
 
 # Générer une paire de clés RSA pour le client
@@ -29,3 +30,25 @@ csr_pem = csr.public_bytes(serialization.Encoding.PEM)
 # Enregistrer le CSR dans un fichier
 with open("csr.pem", "wb") as f:
     f.write(csr_pem)
+
+# Paramètres MQTT
+mqtt_broker_address = "194.57.103.203"
+mqtt_broker_port = 1883
+mqtt_client_id = "ca_client_julien_hugo"
+topic = "vehicule/JH"
+
+USE_VERSION2_CALLBACKS = not paho.mqtt.__version__.startswith("1.")
+
+if USE_VERSION2_CALLBACKS:
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, mqtt_client_id)
+else:
+    client = mqtt.Client(mqtt_client_id)
+
+print("connecting to broker")
+
+if client.connect("194.57.103.203",1883,60) != 0:
+    print("Problème de connexion avec le broker")
+
+client.publish(topic, "test1")
+print("envoie message test 1")
+client.loop_forever()
